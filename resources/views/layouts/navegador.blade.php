a0abd696a68619440e8702addf777afbd4924445
Giusti89 <giustiever17@gmail.com>
2024-11-28 18:13:02 -0400

    correccion dispositivos moviles

--- resources/views/layouts/navegador.blade.php
+++ resources/views/layouts/navegador.blade.php
@@ -1,23 +1,41 @@
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
+    <!-- Botón de colapso (si lo necesitas para el pushmenu en AdminLTE) -->
     <ul class="navbar-nav">
         <li class="nav-item">
             <a class="nav-link" data-widget="pushmenu" href="#" role="button">
                 <i class="fas fa-bars"></i>
             </a>
         </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            <a href="{{route('dashboard')}}" class="nav-link">Inicio</a>
+    </ul>
+
+    <!-- Menú para pantallas grandes -->
+    <ul class="navbar-nav d-none d-sm-flex">
+        <li class="nav-item">
+            <a href="{{ route('dashboard') }}" class="nav-link">Inicio</a>
         </li>
-
-        <li class="nav-item d-none d-sm-inline-block">
-            <a href="{{route('inicioPrestamos')}}" class="nav-link">Prestamos</a>
-        </li>           
-
-        <li class="nav-item d-none d-sm-inline-block">
-            <a href="{{route('ordepagoIndex')}}" class="nav-link"> Orden Pagos</a>
+        <li class="nav-item">
+            <a href="{{ route('inicioPrestamos') }}" class="nav-link">Préstamos</a>
+        </li>
+        <li class="nav-item">
+            <a href="{{ route('ordepagoIndex') }}" class="nav-link">Orden Pagos</a>
         </li>
     </ul>
+
+    <!-- Menú desplegable para pantallas pequeñas -->
+    <ul class="navbar-nav d-block d-sm-none">
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                Menú
+            </a>
+            <div class="dropdown-menu">
+                <a href="{{ route('dashboard') }}" class="dropdown-item">Inicio</a>
+                <a href="{{ route('inicioPrestamos') }}" class="dropdown-item">Préstamos</a>
+                <a href="{{ route('ordepagoIndex') }}" class="dropdown-item">Orden Pagos</a>
+            </div>
+        </li>
+    </ul>
+
+    <!-- Botón de cerrar sesión -->
     <ul class="navbar-nav ml-auto">
         <form method="POST" action="{{ route('logout') }}">
             @csrf
@@ -29,3 +47,5 @@
         </form>
     </ul>
 </nav>
+
+
